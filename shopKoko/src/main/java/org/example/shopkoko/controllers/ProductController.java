d790580c3d4b3664c7791b7cae81306e5b5bb393
Yergeshbay <yy1343@rit.edu>
2026-03-15 11:32:18 +0400

    Controller update
    added addproducts page and its mapping in the controller.

---
+++ shopKoko/src/main/java/org/example/shopkoko/controllers/ProductController.java
@@ -0,0 +1,35 @@
+package org.example.shopkoko.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.example.shopkoko.services.ProductService;
+
+@Controller
+public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String getProducts(Model model) {
+        model.addAttribute("productList", this.productService.findALl());
+        return "products";
+    }
+    @GetMapping("/products/add")
+    public String addProducts(Model model) {
+        model.addAttribute("productList", this.productService.findALl());
+        return "addProducts";
+    }
+
+    @PostMapping("/products/add")
+    public String saveProducts(Model model) {
+        model.addAttribute("productList", this.productService.findALl());
+        return "redirect:addProducts";
+    }
+}
