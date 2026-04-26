ff799e377f89f35d21628f6068b49f9fdec0f65c
Tanmay-1210 <tanupatil04@gmail.com>
2026-04-26 19:46:49 +0400

    complete OrderRestController file
    This contains the code needed to make the page fully functional. Also added my name to each file

--- shopKoko/src/main/java/org/example/shopkoko/repositories/OrderRepository.java
+++ shopKoko/src/main/java/org/example/shopkoko/repositories/OrderRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import jakarta.transaction.Transactional;
 import java.util.List;
 
+/*Made by Tanmay*/
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByStatus(String status);
